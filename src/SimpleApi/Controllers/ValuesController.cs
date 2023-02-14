e73ebd487e93e04cfd7f525b9c4b74e103c73145
Ashish <raj.arora1103@gmail.com>
2023-02-14 21:45:36 +0530

    change values in valuesController

--- src/SimpleApi/Controllers/ValuesController.cs
+++ src/SimpleApi/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace SimpleApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Dotnet", "Azure" };
         }
 
         // GET api/values/5
